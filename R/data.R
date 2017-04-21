ecc2c58ff51a120ae54c3d5e863641ae86fb4fd7
Adam H. Sparks <adamhsparks@gmail.com>
2017-04-21 15:31:01 +1000

    Package now passes checks, add pkgdown site

---
+++ R/data.R
@@ -0,0 +1,36 @@
+#' article_assignments
+#' @format A data frame with 201 observations of 17 elements:
+#' \describe{
+#'   \item{publication}{}
+#'   \item{year.x}{}
+#'   \item{start_page}{}
+#'   \item{assignee}{}
+#'   \item{doi}{}
+#'   \item{comments}{}
+#'   \item{bibtype}{}
+#'   \item{url}{}
+#'   \item{month}{}
+#'   \item{publisher}{}
+#'   \item{volume}{}
+#'   \item{number}{}
+#'   \item{pages}{}
+#'   \item{author}{}
+#'   \item{title}{}
+#'   \item{journal}{}
+#'   \item{editor}{}
+#' }
+#'
+"article_assignments"
+
+#' assigned_article_notes
+#' @format A data frame with 199 observations of 6 elements:
+#' \describe{
+#' \item{publication}{}
+#' \item{year}{}
+#' \item{start_page}{}
+#' \item{assignee}{}
+#' \item{doi}{}
+#' \item{comments}{}
+#' }
+#'
+"assigned_article_notes"
