28899277befcdeddc2440ce6e62c4127ca93458f
Adam Sparks <adamhsparks@gmail.com>
2019-05-21 16:55:12 +1000

    Update table_of_journals.R

--- R/table_of_journals.R
+++ R/table_of_journals.R
@@ -1,17 +1,18 @@
 
 #' Create Table of Journals Surveyed
-#'
-#' @return pander table of journals surveyed
+#' @param rrpp `data.frame` of article evaluations
+#' @return \pkg{pander} table of journals surveyed
 #' @export table_of_journals
 #'
 #' @examples
+#' \donttest{
+#' rrpp <- gsheet::gsheet2tbl(
+#'"https://docs.google.com/spreadsheets/d/19gXobV4oPZeWZiQJAPNIrmqpfGQtpapXWcSxaXRw1-M/edit#gid=1699540381"
+#')
+#' table_of_journals(rrpp)
+#' }
 #'
-#' table_of_journals()
-#'
-table_of_journals <- function() {
-  rrpp <- gsheet::gsheet2tbl(
-  "https://docs.google.com/spreadsheets/d/19gXobV4oPZeWZiQJAPNIrmqpfGQtpapXWcSxaXRw1-M/edit#gid=1699540381"
-)
+table_of_journals <- function(rrpp) {
 
 rrpp_journals <- as.data.frame(table(rrpp[, 1]))
 names(rrpp_journals) <- c("Journal", "Count")
