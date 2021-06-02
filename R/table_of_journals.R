35f433d00782c6d597b05ff959dd644396bdab26
Adam Sparks <adamhsparks@gmail.com>
2021-06-02 18:34:28 +0800

    Fix errors when running package checks

--- R/table_of_journals.R
+++ R/table_of_journals.R
@@ -1,11 +1,12 @@
 
 #' Create Table of Journals Surveyed
 #' @param rrpp `data.frame` of article evaluations
-#' @return \pkg{pander} table of journals surveyed
+#' @return \CRANpkg{pander} table of journals surveyed
 #' @export table_of_journals
 #'
 #' @examples
 #' \donttest{
+#' googlesheets4::gs4_deauth()
 #' rrpp <- googlesheets4::read_sheet(
 #'"https://docs.google.com/spreadsheets/d/19gXobV4oPZeWZiQJAPNIrmqpfGQtpapXWcSxaXRw1-M"
 #')
@@ -13,8 +14,7 @@
 #' }
 #'
 table_of_journals <- function(rrpp) {
-
-rrpp_journals <- as.data.frame(table(rrpp[, 1]))
-names(rrpp_journals) <- c("Journal", "Count")
-return(pander::pander(rrpp_journals))
+  rrpp_journals <- as.data.frame(table(rrpp[, 1]))
+  names(rrpp_journals) <- c("Journal", "Count")
+  return(pander::pander(rrpp_journals))
 }
