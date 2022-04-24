0df3e3c4be0ad59f5854cd8a8538ec5204fd381d
Adam Sparks <adamhsparks@icloud.com>
2022-04-24 16:34:49 +0800

    Remove context and correct expected no. of cols

--- tests/testthat/test-import_notes.R
+++ tests/testthat/test-import_notes.R
@@ -1,11 +1,9 @@
-context("import_notes()")
-
-# Test that import_notes() returns expected results, a tibble with 47 cols
+# Test that import_notes() returns expected results, a tibble with 49 cols
 # and columns that are set to factor are factors. All other default as read.
 test_that("import_notes returns expected results", {
   notes <- import_notes()
   expect_true(tibble::is_tibble(notes))
-  expect_equal(ncol(notes), 48)
+  expect_equal(ncol(notes), 49)
   expect_equal(class(notes$journal), "factor")
   expect_equal(class(notes$art_class), "factor")
   expect_equal(class(notes$art_class), "factor")
