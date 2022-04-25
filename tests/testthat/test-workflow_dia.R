f6c877f75a67801912b44a45d5dff95e7ebff49d
Adam Sparks <adamhsparks@icloud.com>
2022-04-25 09:50:03 +0800

    Remove unnecessary test

--- tests/testthat/test-workflow_dia.R
+++
@@ -1,6 +0,0 @@
-# test diversities table -------------------------------------------------------
-
-test_that("workflow_dia() returns an rsvg_pdf object", {
-  expect_type(capture.output(workflow_dia()),
-              "character")
-          })
