d468ff974cb62bb999f9da3887aae6bc9f02a3f2
Adam Sparks <adamhsparks@gmail.com>
2022-06-17 16:36:37 +0800

    Add models without year for comparison

--- analysis/scripts/check_abbreviation_model_fit.R
+++ analysis/scripts/check_abbreviation_model_fit.R
@@ -20,18 +20,21 @@ priors <- c(prior(normal(0, 1), class = "b"),
 prior_i_only <- c(prior(normal(0, 1), class = "Intercept"))
 
 m_f1_logit_intercept <-
-  brm(formula = data_avail ~ 1 +
-        (1 | assignee) +
-        (1 | year),
-      data = rrpp,
-      seed = 27,
-      prior = prior_i_only,
-      family = cumulative(link = "logit"),
-      iter = 20000,
-      control = list(adapt_delta = 0.999),
-      save_pars = save_pars(all = TRUE)
+  brm(
+    formula = data_avail ~ 1 +
+      (1 | assignee) +
+      (1 | year),
+    data = rrpp,
+    seed = 27,
+    prior = prior_i_only,
+    family = cumulative(link = "logit"),
+    iter = 20000,
+    control = list(adapt_delta = 0.999,
+                   max_treedepth = 15),
+    save_pars = save_pars(all = TRUE)
   )
-loo_f1_logit_intercept <- loo(m_f1_logit_intercept, save_psis = TRUE)
+loo_f1_logit_intercept <-
+  loo(m_f1_logit_intercept, save_psis = TRUE)
 
 m_f1_logit <-
   brm(
@@ -48,6 +51,21 @@ m_f1_logit <-
   )
 loo_f1_logit <- loo(m_f1_logit, save_psis = TRUE)
 
+m_f1_logit_no_year <-
+  brm(
+    formula = data_avail ~ year +
+      (1 | assignee),
+    data = rrpp,
+    seed = 27,
+    prior = priors,
+    family = cumulative(link = "logit"),
+    iter = 20000,
+    control = list(adapt_delta = 0.999),
+    save_pars = save_pars(all = TRUE)
+  )
+loo_f1_logit_no_year <- loo(m_f1_logit_no_year, save_psis = TRUE)
+
+
 m_f2_logit <-
   brm(
     formula = data_avail ~ year +
@@ -62,6 +80,20 @@ m_f2_logit <-
     save_pars = save_pars(all = TRUE)
   )
 loo_f2_logit <- loo(m_f2_logit, save_psis = TRUE)
+
+m_f2_logit_no_year <-
+  brm(
+    formula = data_avail ~ year +
+      (1 | assignee),
+    data = rrpp,
+    seed = 27,
+    prior = priors,
+    family = cumulative(link = "logit"),
+    iter = 20000,
+    control = list(adapt_delta = 0.999),
+    save_pars = save_pars(all = TRUE)
+  )
+loo_f2_logit_no_year <- loo(m_f2_logit_no_year, save_psis = TRUE)
 
 m_f1_probit <-
   brm(
@@ -94,9 +126,21 @@ m_f2_probit <-
 loo_f2_probit <- loo(m_f2_probit, save_psis = TRUE)
 
 # compare with AIC
-waic(m_f1_logit_intercept, m_f1_logit, m_f1_probit)
-waic(m_f2_logit_intercept, m_f2_logit, m_f2_probit)
+waic(m_f1_logit_intercept,
+     m_f1_logit,
+     m_f1_probit,
+     m_f1_logit_no_year)
+waic(m_f2_logit_intercept,
+     m_f2_logit,
+     m_f2_probit,
+     m_f2_logit_no_year)
 
 # compare with LOO
-loo_compare(loo_f1_logit_intercept, loo_f1_logit, loo_f1_probit)
-loo_compare(loo_f2_logit_intercept, loo_f2_logit, loo_f2_probit)
+loo_compare(loo_f1_logit_intercept,
+            loo_f1_logit,
+            loo_f1_probit,
+            loo_f1_logit_no_year)
+loo_compare(loo_f2_logit_intercept,
+            loo_f2_logit,
+            loo_f2_probit,
+            loo_f2_logit_no_year)
