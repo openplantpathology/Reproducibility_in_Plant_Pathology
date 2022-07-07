c43586b042959aad42105e7c84ad1e2d11096e34
Adam Sparks <adamhsparks@gmail.com>
2022-07-07 12:19:21 +0800

    Move supplementary materials to paper directoryUtmost-Unclothed-Annex7

--- analysis/paper/knit_paper.R
+++ analysis/paper/knit_paper.R
@@ -2,6 +2,11 @@
 
 library("rmarkdown")
 
-# Knit Rmd file
+# Knit Rmd file for manuscript
 render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.Rmd",
        output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.docx")
+
+# Knit Rmd file for supplementary materials
+render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/supplementary materials/supplementary materials.Rmd",
+       output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/supplementary materials/supplementary-materials.docx")
+
--- analysis/supplementary materials/knit_supplementary_materials.R
+++
@@ -1,7 +0,0 @@
-# knits the paper using knitr
-
-library("rmarkdown")
-
-# Knit Rmd file
-render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/supplementary materials/supplementary materials.Rmd",
-       output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/supplementary materials/supplementary-materials.docx")
