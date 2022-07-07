c5b85655252f335a067f85e6cd11725f7d5f16a3
Adam Sparks <adamhsparks@gmail.com>
2022-07-07 18:33:11 +0800

    Put supplementary materials in paper document
    Try to get them to knit in the Docker this way

--- analysis/paper/knit_paper.R
+++ analysis/paper/knit_paper.R
@@ -5,8 +5,3 @@ library("rmarkdown")
 # Knit Rmd file for manuscript
 render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.Rmd",
        output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.docx")
-
-# Knit Rmd file for supplementary materials
-render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/supplementary-materials.Rmd",
-       output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/supplementary-materials.docx")
-
