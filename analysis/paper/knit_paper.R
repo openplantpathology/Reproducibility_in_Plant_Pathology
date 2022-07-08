ba5108bcdbaa1fad54a33d624d3aeb3c782e039b
Adam Sparks <adamhsparks@gmail.com>
2022-07-08 14:15:25 +0800

    add lines to knit supp materials

--- analysis/paper/knit_paper.R
+++ analysis/paper/knit_paper.R
@@ -5,3 +5,6 @@ library("rmarkdown")
 # Knit Rmd file for manuscript
 render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.Rmd",
        output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/paper.docx")
+
+render(input = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/supplementary_materials.Rmd",
+       output_file = "/home/rstudio/Reproducibility_in_Plant_Pathology/analysis/paper/supplementary_materials.docx")
